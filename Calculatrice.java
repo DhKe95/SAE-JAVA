93e128f9170465e96cc7fce3f5c954757da45934
Gnanapragasam Royston <158213766+GnanapragasamRoyston@users.noreply.github.com>
2024-05-29 22:27:17 +0200

    Add files via upload

---
+++ Calculatrice.java
@@ -0,0 +1,21 @@
+public class Calculatrice {
+    public static void main(String[] args) throws Exception {
+        System.out.println("début du programme");
+
+        Expression deux = new Nombre(2) ;
+        Expression trois = new Nombre(3) ;
+        Expression dixSept = new Nombre(17) ;
+        Expression s = new Soustraction(dixSept, deux) ;
+        Expression a = new Addition(deux, trois) ;
+        try{
+            Expression d = new Division(s, a) ;; // doit afficher une erreur
+            System.out.println(d + " = " + d.valeur()) ; // affiche ((17 - 2) / (2 + 3)) = 3
+            }
+            catch(Exception er)
+            {
+                System.err.println("Erreur:" + er.getMessage());
+            }
+
+        System.out.println("fin du programme");
+    }
+}
