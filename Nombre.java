e2c1c6d96a052b780b89a13cd55588039f82bc1b
Gnanapragasam Royston <158213766+GnanapragasamRoyston@users.noreply.github.com>
2024-06-20 22:18:14 +0200

    ajout de commentaire

--- Nombre.java
+++ Nombre.java
@@ -2,21 +2,32 @@ public class Nombre extends Expression{
 
     private int valeurNombre ;
 
-
+    /*
+     * Constructeur vide
+     */
     public Nombre()
     {
     }
 
+    /*
+     * Constructeur champs à champs
+     */
     public Nombre(int nombre)
     {
         this.valeurNombre= nombre;
     }
 
+    /*
+     * retourne la variable d'instance
+     */
     public double valeur()
     {
         return this.valeurNombre;
     }
 
+    /*
+     * retourne une chaine de caractères representant le nombre
+     */
     public String toString()
     {
         return " " + this.valeur() + " ";
