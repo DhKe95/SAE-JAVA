a43c337c8175db59c0d64ed75a3acb9bafd54340
Gnanapragasam Royston <158213766+GnanapragasamRoyston@users.noreply.github.com>
2024-06-20 22:34:37 +0200

    ajout de commentaire

--- Operation.java
+++ Operation.java
@@ -2,17 +2,27 @@ public abstract class Operation extends Expression{
     private Expression Operande1;
     private Expression Operande2;
 
+    /*
+     * Constructeur vide
+     */
     public Operation()
     {}
 
+    /*
+     * Constructeur champs à champs
+     */
     public Operation(Expression valeur1,Expression valeur2)
     {
         this.Operande1= valeur1;
         this.Operande2= valeur2;
     }
 
+    /*
+     * Méthode abstraite
+     */
     public abstract double valeur();
 
+    
     public Expression getOperande1()
     {
 
